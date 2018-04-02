2a371ea0dbe0cf0e228c980d57fc6883cc963d2a
Y. S <ThePhantomGamer@users.noreply.github.com>
2018-04-02 14:18:01 -0400

    Add files

---
+++ CLUtil.py
@@ -0,0 +1,111 @@
+import random
+import time
+
+
+class Timer:
+
+    def __init__(self, delay, label):
+        self.delay = delay  # seconds
+        self.t_end = -1  # always returns true if not started
+        self.started = False
+        self.label = label
+        self.start_time = -1
+        if dbg:
+            print("{0} has started: {1}".format(label, self.started))
+
+    def start(self):
+        if self.started and self.done:
+            print('{0} is done'.format(self.label))
+            return
+        elif self.started:
+            if dbg:
+                print('{0} is started, wait'.format(self.label))
+            return
+        else:
+            print("Starting {0}".format(self.label))
+            self.t_end = time.time() + self.delay
+            self.start_time = arrow.now().humanize()
+            self.started = True
+
+    def reset(self):
+        self.started = False
+        if dbg:
+            print("resetting timer")
+
+    @property
+    def time_left(self):
+        return math.floor(self.t_end - time.time())
+
+    @property
+    def done(self):
+        if self.t_end == -1:
+            dbg_inf = inspect.stack()[1]
+            if dbg:
+                print("Error: {0} has not started".format(self.label))
+                print(dbg_inf)
+
+            return False
+        else:
+            if dbg:
+                print("time: {0}\n t_end {1}\n diff: {2}".format(time.time(), self.t_end, time.time() - self.t_end))
+            return time.time() >= self.t_end
+
+
+class FKIO:
+    class Solenoid:
+        def __init__(self, label="Solenoid"):
+            self.label = label
+            self.pushed = False
+
+        def log(self, args):
+            print("{0}: ".format(self.label), args)
+
+        def push(self):
+            if self.pushed:
+                self.log("Already Pushed")
+            else:
+                self.log("pushing out")
+                time.sleep(1.5)
+                self.pushed = True
+
+        def pull(self):
+            if self.pulled:
+                self.log("already pulled")
+            else:
+                self.log("pulling in")
+                time.sleep(0.5)
+                self.pushed = False
+
+        @property
+        def pulled(self):
+            return not self.pushed
+
+    class Button:
+        def __init__(self, label="Button"):
+            self.label = label
+
+        @property
+        def is_pressed(self):
+            # todo implement
+            # return bool(random.randint(0, 1))
+            return False  # for now no emergency
+
+    def __init__(self):
+        self.ls1 = False
+        self.ls2 = False
+        self.solenoid1 = self.Solenoid("Main Crusher")
+        self.solenoid2 = self.Solenoid("Can Pusher")
+        self.emergency_btn = self.Button("Emergency Stop Button")
+
+    def chk_emrg(self):
+        return self.emergency_btn.is_pressed
+
+    def fkio_cycle(self):
+        if random.random() * 50 < 15:
+            self.ls1 = True
+        elif random.random() * 50 > 15:
+            self.ls1 = False
+
+    @staticmethod
+    def cleanup():
+        print("Cleaning up FKIO")
---
+++ CanCrusher.py
@@ -0,0 +1,117 @@
+import time
+from CLUtil import Timer, FKIO
+
+# from CLUtil import once
+
+dbg = True
+
+MOVE_TIME = 2
+CRUSH_TIME = 2
+PUSH_TIME = 2
+
+
+# doing_nothing is general, for when there is no cans etc.
+
+# moving_can is for when the robotic arm running, moving the can from the feeder into the crushing area once the
+# robot has dropped the can, it hits the limit switch, which triggers a timer which then triggers crushing_can
+
+# crushing_can is for after the allotted time has elapsed. the main cylinder solenoid will be triggered to
+# extend, wait 0.7 secs then pulls back in. after that, the small piston will be triggered and will push the
+# crushed can into the slit, which the inclined plane will carry down
+
+class TestCrusher:
+    states = ['doing_nothing', 'moving_can', 'crushing_can', 'pushing_can', 'end_state']
+    state = None
+    io = FKIO()
+    __cnt = 1
+    move_timer = Timer(MOVE_TIME, "Move Timer")  # assumed arbitrary amount of time it will take to "move" the can
+    crush_timer = Timer(CRUSH_TIME, "Crush Timer")
+    push_timer = Timer(PUSH_TIME, "Push Timer")
+    crush_delay = Timer(3, "Delay Crush")
+    push_delay = Timer(1, "Push Delay")
+
+    def reset_timers(self):
+        self.move_timer.reset()
+        self.crush_timer.reset()
+        self.push_timer.reset()
+        self.crush_delay.reset()
+        self.push_delay.reset()
+
+    def __init__(self):
+        self.state = self.states[0]  # initial state
+
+    def incr_can(self):
+        print("Current can count:", self.__cnt)
+        self.__cnt += 1
+
+    def run(self):
+        self.io.fkio_cycle()
+        emerg = self.io.chk_emrg()  # emergency button
+        if emerg:
+            self.state = 'end_state'
+
+        # periodically update stuff like if can limit switch has been pressed here
+        if self.state is 'doing_nothing':
+            if self.io.ls1:  # limit switch is pressed
+                self.state = self.states[1]
+            print("doin nothing lmao")
+
+        elif self.state is 'moving_can':
+            self.move_timer.start()
+            # print("Moving can\n{0} Seconds Remaining".format(self.move_timer.time_left))
+
+            if self.move_timer.done:
+                print("Done moving can, next state")
+                self.state = self.states[2]
+
+        elif self.state is 'crushing_can':
+
+            self.crush_delay.start()
+
+            # todo add limit switch dependency
+            if self.crush_timer.done:
+                print("Done crushing can")
+                self.io.solenoid1.pull()
+                self.state = self.states[3]
+            else:
+                if self.crush_delay.done:
+                    print("Crushing can")
+                    self.crush_timer.start()
+                    self.io.solenoid1.push()
+
+        elif self.state is 'pushing_can':
+            self.push_delay.start()
+            self.push_timer.start()
+            if self.push_timer.done:
+                print("Done pushing can down")
+                self.incr_can()
+                self.io.solenoid2.pull()  # make sure this stuff happens
+                self.reset_timers()
+                self.state = 'doing_nothing'  # go back to waiting for another can
+
+            else:
+                if self.push_delay.done:
+                    self.io.solenoid2.push()
+
+        elif self.state is 'end_state':
+            if emerg:
+                print("Stopped Due to Emergency Stop btn")
+                # todo add stop robot code here
+            print("Cans Crushed:", self.__cnt)
+            self.io.cleanup()
+            self.reset_timers()  # just for fun
+
+
+def main():
+    # crusher = CanCrusher()
+    crusher = TestCrusher()
+    while True:
+        time.sleep(0.1)
+        crusher.run()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Exitting")
